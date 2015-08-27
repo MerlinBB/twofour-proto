5c3f277d8cf375c6a71aa4ae1317b815e74241d4
Merlin Mason <info@merlinmason.co.uk>
2015-08-27 14:45:04 +0100

    animate in bars and handle answers

--- js/project.js
+++ js/project.js
@@ -11,8 +11,29 @@
                 { title: "He-Man", correct: false },
                 { title: "Bananaman", correct: false }
             ]
+        },
+        {
+            question: "Letters",
+            num: 2,
+            answers: [
+                { title: "A", correct: true },
+                { title: "2", correct: false },
+                { title: "C", correct: true },
+                { title: "7", correct: false }
+            ]
+        },
+        {
+            question: "Numbers",
+            num: 3,
+            answers: [
+                { title: "9", correct: true },
+                { title: "2", correct: true },
+                { title: "C", correct: false },
+                { title: "7", correct: true }
+            ]
         }
     ];
+
 
 
     var speed;
@@ -21,9 +42,21 @@
     var rows = 10;
     var score = 0;
     var incorrectAnswers = 0;
+    var currentCorrectAnswers = 0;
+
+    var answerT = "" +
+    "<div class=\"bar\">" +
+        "<div class=\"inner\">" +
+            "<% _.each(answers, function (a, i) { %>" +
+                "<div class=\"answer\" data-i=\"<%- i+1 %>\" data-correct=\"<%- a.correct %>\"><%- a.title %></div>" +
+            "<% }); %>" +
+        "</div>" +
+    "</div>";
+
     var tetris = {
 
         init: function () {
+            questions = _.shuffle(questions);
             this.bindUIActions();
             this.setSpeed();
             this.setRowsToFail();
@@ -34,7 +67,7 @@
             $(".start").on("click", function () { tetris.start(); });
             $(".speed").on("change", function () { tetris.setSpeed(); });
             $(".rows").on("change", function () { tetris.setRowsToFail(); });
-            $(".answer").on("click", function (e) { tetris.answerClicked(e); });
+            $("body").on("click", ".answer", function (e) { tetris.answerClicked(e); });
             $(window).on("keyup", function (e) { tetris.keyHit(e); });
         },
 
@@ -49,9 +82,19 @@
         clear: function () {
             score = 0;
             tetris.updateScore();
+
+            question = 0;
+            incorrectAnswers = 0;
+
+            questions = _.shuffle(questions);
+
+            $(".question .inner").text("");
+            $(".question-sub .inner").text("");
+            $(".stage").empty();
         },
 
         start: function () {
+            tetris.updateScore();
             tetris.newQuestion();
         },
 
@@ -60,7 +103,11 @@
 
             // if there a question left?
             if (q) {
-
+                $(".question .inner").text(q.question);
+                $(".question-sub .inner").text(q.num);
+                var template = _.template(answerT);
+                $(".stage").prepend(template(q));
+                tetris.animateBar();
             } else {
                 tetris.gameover();
             }
@@ -68,32 +115,81 @@
 
         keyHit: function (e) {
             var answer;
-            tetris.answerChosen(answer);
+
+            switch (e.keyCode) {
+            case 49: // 1
+            case 65: // a
+                answer = 1;
+                break;
+
+            case 50: // 2
+            case 83: // s
+                answer = 2;
+                break;
+
+            case 51: // 3
+            case 68: // d
+                answer = 3;
+                break;
+
+            case 52: // 4
+            case 70: // f
+                answer = 4;
+                break;
+            }
+
+            if (answer) {
+                tetris.answerChosen(answer);
+            }
         },
 
         answerClicked: function (e) {
-            var answer;
+            var answer = $(e.currentTarget).data("i");
             tetris.answerChosen(answer);
         },
 
-        answerChosen: function (a) {
-
+        answerChosen: function (i) {
+            var answer = $(".answer:nth-child(" + i + ")");
+            if ($(answer).data("correct")) {
+                $(answer).addClass("correct");
+                tetris.correct();
+            } else {
+                tetris.incorrect();
+            }
         },
 
         incorrect: function () {
-            incorrectAnswers++;
-
             if (incorrectAnswers === rowsToFail) {
                 tetris.gameover();
+            } else {
+                var pos = (incorrectAnswers * 10) + "%";
+                $(".stage .bar:first").stop().animate({ bottom: pos }, 400).addClass("incorrect");
+
+                incorrectAnswers += 1;
+
+                question++;
+                tetris.newQuestion();
             }
         },
 
         correct: function () {
+            var q = questions[question];
 
-            score++;
-            tetris.updateScore();
+            currentCorrectAnswers += 1;
 
-            tetris.newQuestion;
+            if (currentCorrectAnswers === q.num) {
+                // all answers given - next
+                score++;
+                tetris.updateScore();
+
+                $(".stage .bar:first").stop().animate({ left: "100%"}, 400, function () {
+                    $(this).remove();
+                    question++;
+                    tetris.newQuestion();
+                });
+            } else {
+                // there's still more to be done
+            }
         },
 
         gameover: function () {
@@ -102,6 +198,18 @@
 
         updateScore: function () {
             $(".score").text(score);
+        },
+
+        animateBar: function () {
+            var bar = $(".stage .bar:first");
+            var end = (incorrectAnswers * 10) + "%";
+            var time = (rows - incorrectAnswers) * speed;
+
+            console.log(end);
+
+            $(bar).animate({ bottom: end }, time,  "linear", function () {
+                tetris.incorrect();
+            });
         }
     };
 
